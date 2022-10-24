0e4da55c2c1a8df976ee60acc41b88d7e6a4fa01
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-24 20:16:26 -0300

    fix: update configs tailwind and dependencies

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,7 @@
-import { useTheme } from 'contexts/Theme';
-
-import styles from './Home.module.scss';
-
 export function Homepage() {
-  const { theme } = useTheme();
-
   return (
-    <>
-      <div className={styles.home} data-theme={theme}>
-        <h1>
-          vite<span>boilerplate 🤙🏾</span>
-        </h1>
-      </div>
-    </>
+    <div>
+      <h1>nv99 badges</h1>
+    </div>
   );
 }
