3f79eebabf8e921f1339b7e5d5d16c5b5845b24c
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-25 01:50:47 -0300

    feat: add services api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `http://localhost:3333`,
+  baseURL: `https://stickers-flow3r-2eqj3fl3la-ue.a.run.app`,
+});
+
+export const badgeApi = axios.create({
+  baseURL: `https://stickers-nv99-2eqj3fl3la-ue.a.run.app///v1/market/feed`,
 });
