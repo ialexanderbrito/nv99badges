e93ab504b968a11ecaa3086db4d9f45045f90fb0
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-25 13:41:30 -0300

    feat: add new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 
+import { Badge } from 'pages/Badge';
 import { Homepage } from 'pages/Home';
+import { NotFound } from 'pages/NotFound';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/mais-raros" element={<Homepage />} />
+      <Route path="/mais-resgatados" element={<Homepage />} />
+      <Route path="/mais-recentes" element={<Homepage />} />
+      <Route path="/badge/:code" element={<Badge />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
