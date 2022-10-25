1094274924fb69957501142ee8dc06a66f5e9a11
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-25 01:46:59 -0300

    feat: add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+import logo from 'assets/logo.svg';
+
+export function Header() {
+  return (
+    <header className="bg-primary/90 w-full h-12 flex items-center justify-center fixed top-0 backdrop-blur-2xl ">
+      <Link to="/">
+        <img src={logo} alt="Logo NV99" className="w-9 h-9" />
+      </Link>
+    </header>
+  );
+}
