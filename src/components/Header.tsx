b2f8e32d93eda06d402a55b5a6b0c4191cbf6fa0
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-27 20:04:47 -0300

    fix: header reloading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import logo from 'assets/logo.svg';
 
 export function Header() {
+  const navigate = useNavigate();
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    navigate(0);
+  }
+
   return (
     <header className="bg-primary/90 w-full h-12 flex items-center justify-center fixed top-0 backdrop-blur-2xl ">
-      <Link to="/">
+      <Link
+        to="/"
+        onClick={() => {
+          scrollToTop();
+        }}
+      >
         <img src={logo} alt="Logo NV99" className="w-9 h-9" />
       </Link>
     </header>
