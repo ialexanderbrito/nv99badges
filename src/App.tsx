0827640078a3235c9cb19fe842190a88f9141897
ialexanderbrito <xanderzinho26@gmail.com>
2022-10-28 13:51:46 -0300

    feat: import provider badge

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { MainRoutes } from 'routes';
 
+import { BadgeProvider } from 'contexts/Badges';
 import { ToastProvider } from 'contexts/Toast';
 
 export function App() {
   return (
     <ToastProvider>
       <BrowserRouter>
-        <MainRoutes />
+        <BadgeProvider>
+          <MainRoutes />
+        </BadgeProvider>
       </BrowserRouter>
     </ToastProvider>
   );
