b4a63b1f7fcf2cfd0bb719293fd7bb86231f259f
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-02 00:13:20 -0300

    feat: add input user

--- src/components/FIlter.tsx
+++ src/components/FIlter.tsx
@@ -1,23 +1,50 @@
-import { BiFilterAlt } from 'react-icons/bi';
+import { BiFilterAlt, BiSearch } from 'react-icons/bi';
 
 import { podcastNames } from 'utils/verifyPodcast';
 
 import { useBadges } from 'contexts/Badges';
 
 export function Filter() {
-  const { filter, podcast, setFilter, setPodcast } = useBadges();
+  const {
+    filter,
+    podcast,
+    setFilter,
+    setPodcast,
+    username,
+    setUsername,
+    searchUsername,
+  } = useBadges();
 
   return (
     <>
-      <div className="flex gap-2 w-80 sm:w-3/4 md:w-[900px] ">
+      <div className="w-full justify-evenly gap-2 items-center flex flex-col h-72 md:flex-row md:w-[900px] md:mt-0 md:h-20">
         <button
-          className="bg-primary gap-4 text-white w-60 h-16 flex items-center justify-center rounded-md mb-6 mt-6 md:w-96 hover:bg-nv "
+          className="bg-primary gap-4 text-white w-80 h-16 flex items-center justify-center rounded-md md:w-60 hover:bg-nv "
           onClick={() => {
             setFilter(!filter);
           }}
         >
           <BiFilterAlt />
           Filtrar por podcast
+        </button>
+        <input
+          type="text"
+          placeholder="Pesquisar por usuário"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          className="bg-primary text-white h-16 w-80 rounded-md px-4 outline-none focus:border hover:border border-nv sm:w-full"
+        />
+
+        <button
+          type="button"
+          className="bg-nv text-white w-80 h-14 flex items-center justify-center rounded-md md:w-16"
+          onClick={() => {
+            searchUsername();
+          }}
+        >
+          <BiSearch size={24} />
         </button>
       </div>
 
