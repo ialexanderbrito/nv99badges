5f27cfe01a42272b734d5f9f7f548dea5c3a53f8
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-01 23:49:46 -0300

    feat: add new routes user and count badge user

--- src/server.ts
+++ src/server.ts
@@ -177,6 +177,88 @@ app.get('/badges/search', async (request, response) => {
   }
 });
 
+app.get('/user/:id', async (request, response) => {
+  const { id } = request.params;
+  const { page = 1, limit = 10, order = "asc" } = request.query;
+
+  try {
+    const { data } = await axios.get(`https://stickers-nv99-2eqj3fl3la-ue.a.run.app//v1/badges/return/${id}/list?limit=100000`);
+
+    const badges = data.badges;
+
+    const startIndex = (Number(page) - 1) * Number(limit);
+
+    const endIndex = Number(page) * Number(limit);
+
+    const orderBy = badges.sort((a: any, b: any) => {
+        
+        if (order === 'asc') {
+          return a.count - b.count;
+        }
+  
+        if (order === 'recent') {
+          const dateA = new Date(a.created_at);
+          const dateB = new Date(b.created_at);
+  
+          return dateB.getTime() - dateA.getTime();
+        }
+
+        if(order === 'serial'){
+          return a.serial_number - b.serial_number;
+        }
+  
+        return b.count - a.count;
+      });
+
+    if (startIndex > badges.length) {
+      return response.status(404).json({ message: 'Badges not found' });
+    }
+
+    if (badges.length === 0) {
+      return response.status(404).json({ message: 'Badges not found' });
+    }
+
+    const results: any = {};
+
+    if (endIndex < badges.length) {
+      results.next = {
+        page: Number(page) + 1,
+        limit: Number(limit),
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: Number(page) - 1,
+        limit: Number(limit),
+      };
+    }
+
+    results.results = orderBy.slice(startIndex, endIndex);
+
+    results.results = badges.slice(startIndex, endIndex);
+
+    return response.json(results);
+
+  } catch (error) {
+    return response.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/badge/count/:id', async (request, response) => {
+  const { id } = request.params;
+
+  try {
+    const { data } = await axios.get(`https://stickers-nv99-2eqj3fl3la-ue.a.run.app//v1/badges/count?username=${id}`);
+
+    return response.json(data);
+
+  } catch (error) {
+    return response.status(500).json({ message: 'Internal server error' });
+  }
+
+});
+
 app.listen(process.env.PORT || 3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
