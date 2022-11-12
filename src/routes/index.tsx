a6b805e220538dd12edcb078263650d78c22f455
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-12 19:03:25 -0300

    feat: add route favoritos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { Badge } from 'pages/Badge';
+import { Favoritos } from 'pages/Favoritos';
 import { Homepage } from 'pages/Home';
 import { MaisRecentes } from 'pages/MaisRecentes';
 import { MaisResgatados } from 'pages/MaisResgatados';
@@ -17,6 +18,7 @@ export function MainRoutes() {
       <Route path="/mais-recentes" element={<MaisRecentes />} />
       <Route path="/badge/:code" element={<Badge />} />
       <Route path="/user/:id" element={<User />} />
+      <Route path="/favoritos" element={<Favoritos />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
