57b2d644a7b69d470fcbcab5b68641201468b712
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-11 21:56:25 -0300

    refactor: filter search with press enter and style

--- src/components/FIlter.tsx
+++ src/components/FIlter.tsx
@@ -20,9 +20,9 @@ export function Filter() {
 
   return (
     <>
-      <div className="w-full mt-12 justify-evenly gap-2 items-center flex flex-col h-72 md:flex-row md:w-[900px] md:mt-20 md:h-20">
+      <div className="w-full mt-16 justify-evenly gap-2 items-center flex flex-col h-40 md:flex-row md:w-[900px] md:mt-20 md:h-20">
         <button
-          className="bg-primary gap-1 text-white w-80 h-16 flex items-center justify-center rounded-md md:w-60 hover:bg-nv "
+          className="bg-primary gap-1 text-white w-96 h-16 flex items-center justify-center rounded-md md:w-60 hover:bg-nv "
           onClick={() => {
             setFilter(!filter);
           }}
@@ -31,35 +31,42 @@ export function Filter() {
           Filtrar por podcast
         </button>
 
-        <input
-          type="text"
-          placeholder="Pesquisar por usuário"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-          className="bg-primary text-white h-16 w-80 rounded-md px-4 outline-none focus:border hover:border border-nv sm:w-80 md:w-full"
-        />
+        <div className="flex items-center w-full justify-center gap-2">
+          <input
+            type="text"
+            placeholder="Pesquisar por usuário"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+            onKeyPress={(e) => {
+              if (e.key === 'Enter') {
+                searchUsername();
+              }
+            }}
+            className="bg-primary text-white h-16 w-80 rounded-md px-4 outline-none focus:border hover:border border-nv sm:w-80 md:w-full"
+          />
 
-        <button
-          type="button"
-          className="bg-nv text-white w-80 h-14 flex items-center justify-center rounded-md md:w-16"
-          onClick={() => {
-            searchUsername();
-          }}
-        >
-          {isLoadingPage ? (
-            <Pulsar color="#FFF" size={24} />
-          ) : (
-            <BiSearch size={24} />
-          )}
-        </button>
+          <button
+            type="button"
+            className="bg-nv text-white w-16 h-14 flex items-center justify-center rounded-md md:w-16"
+            onClick={() => {
+              searchUsername();
+            }}
+          >
+            {isLoadingPage ? (
+              <Pulsar color="#FFF" size={24} />
+            ) : (
+              <BiSearch size={24} />
+            )}
+          </button>
+        </div>
       </div>
 
       {filter && (
         <div className="text-white grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5">
           {podcastNames.map((pdc) => (
-            <label className="flex font-bold p-3">
+            <label className="flex font-bold p-3 cursor-pointer">
               <input
                 type="radio"
                 name="podcast"
@@ -75,7 +82,7 @@ export function Filter() {
 
           <button
             type="button"
-            className="flex bg-primary items-center justify-center rounded-sm hover:border-nv hover:border"
+            className="flex bg-primary h-12 items-center justify-center rounded-md hover:bg-nv"
             onClick={() => {
               setFilter(!filter);
               window.location.reload();
