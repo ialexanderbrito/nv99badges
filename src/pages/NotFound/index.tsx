90c443ddf35b147981177ab4df84011389745064
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-11 21:57:50 -0300

    feat: add props page not found

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,16 +2,20 @@ import notFoundImage from 'assets/tela_404.webp';
 
 import { Header } from 'components/Header';
 
-export function NotFound() {
+interface NotFoundProps {
+  title?: string;
+}
+
+export function NotFound({
+  title = 'Infelizmente a página não foi encontrada!',
+}: NotFoundProps) {
   return (
     <>
       <Header />
       <div className="flex bg-dark w-full h-screen  items-center justify-center flex-col">
         <img src={notFoundImage} alt="Página não encontrada" className="w-96" />
 
-        <span className="text-white text-lg mb-4">
-          Infelizmente a página não foi encontrada!
-        </span>
+        <span className="text-white text-lg mb-4 text-center">{title}</span>
         <strong className="text-white">404 NOT FOUND.</strong>
       </div>
     </>
