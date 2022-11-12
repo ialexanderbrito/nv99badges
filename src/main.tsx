0788566caea1f4ae3e2c0045dcd2e6767d16aa7e
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-12 16:54:29 -0300

    fix: react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import './styles/global.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { App } from './App';
 
 const queryClient = new QueryClient();
 
+const devtools = process.env.NODE_ENV === 'development' && (
+  <ReactQueryDevtools initialIsOpen={false} />
+);
+
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      {devtools}
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
