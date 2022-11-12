c4b19983ee3813f641e2a281587156a241da76b6
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-12 17:13:31 -0300

    fix: api user

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -9,7 +9,7 @@ routes.get('/users/:id', async (request, response) => {
   const { page = 1, limit = 10, order = "asc" } = request.query;
 
   try {
-    const { data } = await apiUser.get(`return/${id}/list?limit=50000`);
+    const { data } = await apiBadges.get(`badges/user_badges_search?username=${id}`);
 
     const {data: profile} = await apiBadges.get(`badges/profile_card?username=${id}`);
 
@@ -17,19 +17,24 @@ routes.get('/users/:id', async (request, response) => {
 
     const badges = data.badges;
 
+    let arrayPrincipal: any = [];
+
+    for (const item of badges) {
+      arrayPrincipal = [].concat(arrayPrincipal, item);
+    }
+
     const startIndex = (Number(page) - 1) * Number(limit);
 
     const endIndex = Number(page) * Number(limit);
 
-    const orderBy = badges.sort((a: any, b: any) => {
-
+    const orderBy = arrayPrincipal.sort((a: any, b: any) => {
       if (order === 'asc') {
         return a.count - b.count;
       }
 
       if (order === 'recent') {
-        const dateA = new Date(a.created_at);
-        const dateB = new Date(b.created_at);
+        const dateA = new Date(a.expires_at);
+        const dateB = new Date(b.expires_at);
 
         return dateB.getTime() - dateA.getTime();
       }
@@ -41,17 +46,18 @@ routes.get('/users/:id', async (request, response) => {
       return b.count - a.count;
     });
 
-    if (startIndex > badges.length) {
-      return response.status(404).json({ message: 'Badges not found' });
+
+    if (startIndex > arrayPrincipal.length) {
+      return response.status(404).json({ message: 'Page not found' });
     }
 
-    if (badges.length === 0) {
-      return response.status(404).json({ message: 'Badges not found' });
+    if (orderBy.length === 0) {
+      return response.status(404).json({ message: 'Not found' });
     }
 
     const results: any = {};
 
-    if (endIndex < badges.length) {
+    if (endIndex < arrayPrincipal.length) {
       results.next = {
         page: Number(page) + 1,
         limit: Number(limit),
@@ -67,11 +73,11 @@ routes.get('/users/:id', async (request, response) => {
 
     results.results = orderBy.slice(startIndex, endIndex);
 
-    results.results = badges.slice(startIndex, endIndex);
+    results.results = arrayPrincipal.slice(startIndex, endIndex);
 
     results.profile = profileStats;
 
-    return response.json(results);
+    return response.status(200).json(results);
 
   } catch (error) {
     return response.status(500).json({ message: 'Internal server error' });
