05ffa3efbb4f89b24622210ca24630f9289221e3
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-13 22:58:03 -0300

    fix: update favorites list via api

--- src/pages/Favoritos/index.tsx
+++ src/pages/Favoritos/index.tsx
@@ -13,6 +13,8 @@ import { twoDecimals } from 'utils/twoDecimal';
 
 import { useFavorites } from 'hooks/Favorites';
 
+import { getBadgeById } from 'services/get/badges';
+
 export function Favoritos() {
   const navigate = useNavigate();
   const { removeFavorite } = useFavorites();
@@ -22,6 +24,30 @@ export function Favoritos() {
     setFavorites(
       JSON.parse(localStorage.getItem('@nv99badges:favorites') || '[]'),
     );
+  }, []);
+
+  useEffect(() => {
+    const favorites = JSON.parse(
+      localStorage.getItem('@nv99badges:favorites') || '[]',
+    );
+
+    const favoritesCode = favorites.map((favorite: Badge) => favorite.code);
+
+    const promises = favoritesCode.map((code: string) => getBadgeById(code));
+
+    Promise.all(promises).then((badges) => {
+      const newFavorites = badges.map((badge, index) => ({
+        ...badge,
+        ...favorites[index],
+      }));
+
+      setFavorites(newFavorites);
+
+      localStorage.setItem(
+        '@nv99badges:favorites',
+        JSON.stringify(newFavorites),
+      );
+    });
   }, []);
 
   function handleRemoveFavorite(badge: Badge) {
@@ -58,56 +84,54 @@ export function Favoritos() {
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
               {favorites &&
                 favorites.map((favorite) => (
-                  <>
-                    <div
-                      key={favorite.id}
-                      className={cx(
-                        'flex relative gap-2 h-25 md:h-30 w-full bg-primary rounded-none md:rounded ',
-                        {
-                          'border border-nv': favorite?.secret,
-                        },
-                      )}
-                    >
-                      <div className="flex absolute top-1 left-1 z-10 flex-col gap-1"></div>
-                      <img
-                        src={favorite.high}
-                        alt="Avatar"
-                        className="w-32 h-32 rounded"
-                      />
+                  <div
+                    key={favorite.id}
+                    className={cx(
+                      'flex relative gap-2 h-25 md:h-30 w-full bg-primary rounded-none md:rounded ',
+                      {
+                        'border border-nv': favorite?.secret,
+                      },
+                    )}
+                  >
+                    <div className="flex absolute top-1 left-1 z-10 flex-col gap-1"></div>
+                    <img
+                      src={favorite.high}
+                      alt="Avatar"
+                      className="w-32 h-32 rounded"
+                    />
 
-                      <div
-                        className="absolute bg-primary/90 top-0 right-1 p-1 rounded cursor-pointer"
-                        onClick={() => handleRemoveFavorite(favorite)}
-                      >
-                        <p className="font-bold text-sm italic text-white">
-                          Remover
+                    <div
+                      className="absolute bg-primary/90 top-0 right-1 p-1 rounded cursor-pointer"
+                      onClick={() => handleRemoveFavorite(favorite)}
+                    >
+                      <p className="font-bold text-sm italic text-white">
+                        Remover
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center gap-1">
+                      <div className="flex flex-col flex-1 flex-shrink gap-2 p-3 text-white">
+                        <span className="text-md font-semibold text-ui-white line-clamp-1 skeletable">
+                          {favorite?.name}
+                        </span>
+                        <span className="text-sm line-clamp-3">
+                          {favorite?.description}
+                        </span>
+                        <p className="flex gap-2 items-center">
+                          Comprar:
+                          <Link
+                            to={`/badge/${favorite.code}`}
+                            className="flex gap-2 items-center"
+                          >
+                            <Spark />
+                            <span className="font-bold text-nv">
+                              {twoDecimals(favorite?.market_value || 0)}
+                            </span>
+                          </Link>
                         </p>
                       </div>
-
-                      <div className="flex flex-col items-center justify-center gap-1">
-                        <div className="flex flex-col flex-1 flex-shrink gap-2 p-3 text-white">
-                          <span className="text-md font-semibold text-ui-white line-clamp-1 skeletable">
-                            {favorite?.name}
-                          </span>
-                          <span className="text-sm line-clamp-3">
-                            {favorite?.description}
-                          </span>
-                          <p className="flex gap-2 items-center">
-                            Comprar:
-                            <Link
-                              to={`/badge/${favorite.code}`}
-                              className="flex gap-2 items-center"
-                            >
-                              <Spark />
-                              <span className="font-bold text-nv">
-                                {twoDecimals(favorite?.market_value || 0)}
-                              </span>
-                            </Link>
-                          </p>
-                        </div>
-                      </div>
                     </div>
-                  </>
+                  </div>
                 ))}
             </div>
           </>
