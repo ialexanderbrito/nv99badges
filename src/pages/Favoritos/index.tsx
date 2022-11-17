99f7495122c4a960a826685b9bb522cce43990f1
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-17 18:40:43 -0300

    feat: add search badges header

--- src/pages/Favoritos/index.tsx
+++ src/pages/Favoritos/index.tsx
@@ -3,20 +3,28 @@ import { Helmet } from 'react-helmet';
 import { BiArrowBack } from 'react-icons/bi';
 import { Link, useNavigate } from 'react-router-dom';
 
+import { useQuery } from '@tanstack/react-query';
 import { Spark } from 'assets/Spark';
 import cx from 'classnames';
 import { Badge } from 'types/BadgesProps';
 
+import { Card } from 'components/Card';
+import { CardSkeleton } from 'components/CardSkeleton';
 import { Header } from 'components/Header';
 
 import { twoDecimals } from 'utils/twoDecimal';
 
+import { useBadges } from 'contexts/Badges';
+import { useToast } from 'contexts/Toast';
+
 import { useFavorites } from 'hooks/Favorites';
 
-import { getBadgeById } from 'services/get/badges';
+import { getBadgeById, getBadgesSearch } from 'services/get/badges';
 
 export function Favoritos() {
   const navigate = useNavigate();
+  const { toast } = useToast();
+  const { badgesFiltered, searchBadge, setBadgesFiltered } = useBadges();
   const { removeFavorite } = useFavorites();
   const [favorites, setFavorites] = useState<Badge[]>([]);
 
@@ -56,6 +64,25 @@ export function Favoritos() {
     );
   }
 
+  const { isLoading: isLoadingSearch } = useQuery(
+    ['search', searchBadge],
+    () => getBadgesSearch(searchBadge),
+    {
+      onSuccess: (data) => {
+        setBadgesFiltered(data.data);
+      },
+      onError: () => {
+        toast.error('Badges not found');
+      },
+      staleTime: 100000,
+      enabled: searchBadge !== '',
+    },
+  );
+
+  const verificaBusca = isLoadingSearch && searchBadge !== '';
+  const verificaBadgesBuscadas =
+    badgesFiltered?.length > 0 && searchBadge.length > 0;
+
   return (
     <>
       <Helmet>
@@ -69,70 +96,113 @@ export function Favoritos() {
           <BiArrowBack size={32} onClick={() => navigate(-1)} />
         </div>
 
-        {favorites.length === 0 ? (
-          <div className="flex flex-col items-center h-[850px] justify-center">
-            <h1 className="text-white text-2xl font-bold mt-4 mb-4">
-              Você não tem nenhum emblema favoritado
-            </h1>
-          </div>
-        ) : (
+        {verificaBusca ? (
           <>
             <h1 className="text-white text-2xl font-bold mt-4 mb-4">
-              Seus emblemas favoritos
+              Resultados para "{searchBadge}"
             </h1>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-              {favorites &&
-                favorites.map((favorite) => (
-                  <div
-                    key={favorite.id}
-                    className={cx(
-                      'flex relative gap-2 h-25 md:h-30 w-full bg-primary rounded-none md:rounded ',
-                      {
-                        'border border-nv': favorite?.secret,
-                      },
-                    )}
-                  >
-                    <div className="flex absolute top-1 left-1 z-10 flex-col gap-1"></div>
-                    <img
-                      src={favorite.high}
-                      alt="Avatar"
-                      className="w-32 h-32 rounded"
+            {Array.from({ length: 2 }).map((_, index) => (
+              <>
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+                  <CardSkeleton key={index} />
+                  <CardSkeleton key={index} />
+                  <CardSkeleton key={index} />
+                </div>
+              </>
+            ))}
+          </>
+        ) : (
+          <>
+            {verificaBadgesBuscadas && (
+              <>
+                <h1 className="text-white text-2xl font-bold mt-4 mb-4">
+                  Resultados para "{searchBadge}"
+                </h1>
+
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+                  {badgesFiltered.map((badge) => (
+                    <Card
+                      badge={badge}
+                      key={badge.id}
+                      onClick={() => {
+                        navigate(`/badge/${badge.code}`);
+                      }}
                     />
+                  ))}
+                </div>
+              </>
+            )}
+          </>
+        )}
 
-                    <div
-                      className="absolute bg-primary/90 top-0 right-1 p-1 rounded cursor-pointer"
-                      onClick={() => handleRemoveFavorite(favorite)}
-                    >
-                      <p className="font-bold text-sm italic text-white">
-                        Remover
-                      </p>
-                    </div>
+        {!verificaBusca && !verificaBadgesBuscadas && (
+          <>
+            {favorites.length === 0 ? (
+              <div className="flex flex-col items-center h-[850px] justify-center">
+                <h1 className="text-white text-2xl font-bold mt-4 mb-4">
+                  Você não tem nenhum emblema favoritado
+                </h1>
+              </div>
+            ) : (
+              <>
+                <h1 className="text-white text-2xl font-bold mt-4 mb-4">
+                  Seus emblemas favoritos
+                </h1>
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+                  {favorites &&
+                    favorites.map((favorite) => (
+                      <div
+                        key={favorite.id}
+                        className={cx(
+                          'flex relative gap-2 h-25 md:h-30 w-full bg-primary rounded-none md:rounded ',
+                          {
+                            'border border-nv': favorite?.secret,
+                          },
+                        )}
+                      >
+                        <div className="flex absolute top-1 left-1 z-10 flex-col gap-1"></div>
+                        <img
+                          src={favorite.high}
+                          alt="Avatar"
+                          className="w-32 h-32 rounded"
+                        />
 
-                    <div className="flex flex-col items-center justify-center gap-1">
-                      <div className="flex flex-col flex-1 flex-shrink gap-2 p-3 text-white">
-                        <span className="text-md font-semibold text-ui-white line-clamp-1 skeletable">
-                          {favorite?.name}
-                        </span>
-                        <span className="text-sm line-clamp-3">
-                          {favorite?.description}
-                        </span>
-                        <p className="flex gap-2 items-center">
-                          Comprar:
-                          <Link
-                            to={`/badge/${favorite.code}`}
-                            className="flex gap-2 items-center"
-                          >
-                            <Spark />
-                            <span className="font-bold text-nv">
-                              {twoDecimals(favorite?.market_value || 0)}
+                        <div
+                          className="absolute bg-primary/90 top-0 right-1 p-1 rounded cursor-pointer"
+                          onClick={() => handleRemoveFavorite(favorite)}
+                        >
+                          <p className="font-bold text-sm italic text-white">
+                            Remover
+                          </p>
+                        </div>
+
+                        <div className="flex flex-col items-center justify-center gap-1">
+                          <div className="flex flex-col flex-1 flex-shrink gap-2 p-3 text-white">
+                            <span className="text-md font-semibold text-ui-white line-clamp-1 skeletable">
+                              {favorite?.name}
                             </span>
-                          </Link>
-                        </p>
+                            <span className="text-sm line-clamp-3">
+                              {favorite?.description}
+                            </span>
+                            <p className="flex gap-2 items-center">
+                              Comprar:
+                              <Link
+                                to={`/badge/${favorite.code}`}
+                                className="flex gap-2 items-center"
+                              >
+                                <Spark />
+                                <span className="font-bold text-nv">
+                                  {twoDecimals(favorite?.market_value || 0)}
+                                </span>
+                              </Link>
+                            </p>
+                          </div>
+                        </div>
                       </div>
-                    </div>
-                  </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+              </>
+            )}
           </>
         )}
       </div>
