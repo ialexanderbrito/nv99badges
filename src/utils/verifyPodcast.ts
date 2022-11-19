11c65fc6ad30e855966b8e0e53460cc703fbaaec
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-19 19:59:58 -0300

    fix: podcast list

--- src/utils/verifyPodcast.ts
+++ src/utils/verifyPodcast.ts
@@ -1,63 +1,67 @@
 export const podcastNames = [
   {
-    id: '25x0SqMaSNXgwv14g0Xg',
-    name: 'FLOW',
-  },
-  {
-    id: '2mXVl9k4sEbrBKrvwDtL',
-    name: 'Flow Games',
-  },
-  {
-    id: 'ac7TKXX0pZ928KiXFzrL',
-    name: 'Kritikê Podcast',
-  },
-  {
-    id: 'mL0SV0pP2YtOeXxjWe2P',
     name: 'VenusPodcast',
+    id: 'mL0SV0pP2YtOeXxjWe2P',
   },
   {
-    id: 'AOncvOxcxNdbLe0IpMxn',
-    name: 'À Deriva',
+    name: 'Flow',
+    id: '25x0SqMaSNXgwv14g0Xg',
   },
   {
-    id: 'XzHIWZr50PSQJ6i47kVd',
     name: 'Flow Sport Club',
+    id: 'XzHIWZr50PSQJ6i47kVd',
   },
   {
-    id: 'Wr8kAod7FUvG4vF8Z1es',
-    name: 'Prosa Guiada',
-  },
-  {
-    id: 'yLTB8ibDV92MmwQMNHQD',
     name: 'Ciência Sem Fim',
+    id: 'yLTB8ibDV92MmwQMNHQD',
   },
   {
-    id: 'P2SEdHsEtEBF4QSglVdv',
+    name: 'Kritikê Podcast',
+    id: 'ac7TKXX0pZ928KiXFzrL',
+  },
+  {
+    name: 'Flow Games',
+    id: '2mXVl9k4sEbrBKrvwDtL',
+  },
+  {
     name: 'Rene e Jotas',
+    id: 'P2SEdHsEtEBF4QSglVdv',
   },
   {
-    id: 'elxu2TNruyiJGBIL56el',
     name: 'Toca do Elfo',
+    id: 'elxu2TNruyiJGBIL56el',
   },
   {
-    id: 'gunlRsDWsJqbk6yp1eNG',
-    name: 'Nerd Sobre Rodas',
+    name: 'Prosa Guiada',
+    id: 'Wr8kAod7FUvG4vF8Z1es',
   },
   {
-    id: 'lnB5tRq9MI5bfPGDOIie',
+    name: 'Estúdios Flow',
+    id: '2QfGAsD7OzgO4TKMg2Hb',
+  },
+  {
     name: 'NV99',
+    id: 'lnB5tRq9MI5bfPGDOIie',
   },
   {
-    id: 'qCvZkm48UzemFtNM1Lh4',
-    name: 'NV99 - Devs',
-  },
-  {
-    id: 'teHRVwzvqpzzLYfTDPK1',
-    name: 'Tonelive',
-  },
-  {
-    id: 'yRIU2ONnLFuaAIA9qQT4',
     name: 'Noir Podcast',
+    id: 'yRIU2ONnLFuaAIA9qQT4',
+  },
+  {
+    name: 'Warriors GH',
+    id: 'RagbjuOaIGgBjnt1470Y',
+  },
+  {
+    name: 'NV99 - Devs',
+    id: 'qCvZkm48UzemFtNM1Lh4',
+  },
+  {
+    name: 'Nerd Sobre Rodas',
+    id: 'gunlRsDWsJqbk6yp1eNG',
+  },
+  {
+    name: 'Tonelive',
+    id: 'teHRVwzvqpzzLYfTDPK1',
   },
 ];
 
