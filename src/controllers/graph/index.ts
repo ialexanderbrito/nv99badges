9874fa2fda4022915019f169ee402bee3d147e20
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-23 12:19:56 -0300

    feat: add graph badge

---
+++ src/controllers/graph/index.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { api } from '../../api';
+
+const routes = Router();
+
+routes.get('/graph', async (request, response) => {
+  const { code } = request.query;
+
+  try {
+    const { data } = await api.get(
+      `market/graph?code=${code}&type=trade&range=7days`,
+    );
+
+    const labels = data.labels;
+    const dataset = data.dataset;
+
+    return response.json({ labels, dataset });
+  } catch (error) {
+    return response.status(404).json({ message: 'Not found' });
+  }
+});
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import badgesRoutes from './controllers/badges';
 import usersRoutes from './controllers/users';
 import creatorsRoutes from './controllers/creators';
+import graphRoutes from './controllers/graph';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(badgesRoutes);
 app.use(usersRoutes);
 app.use(creatorsRoutes);
+app.use(graphRoutes);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log('🚀 Server started on port 3333!');
