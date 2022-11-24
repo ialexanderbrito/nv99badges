1d3d8bf97051cf0cd3b5f1ea98867ba0f2734c45
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-24 19:04:17 -0300

    fix: order badge profile

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import { api } from './../../api';
 import { Router } from 'express';
 
@@ -5,10 +6,18 @@ const routes = Router();
 
 routes.get('/users/:id', async (request, response) => {
   const { id } = request.params;
-  const { page = 1, limit = 10, order = 'asc' } = request.query;
+  const {
+    page = 1,
+    limit = 10,
+    order = 'lower_serial',
+    normal = true,
+    secret = true,
+  } = request.query;
 
   try {
-    const { data } = await api.get(`badges/user_badges_search?username=${id}`);
+    const { data } = await api.get(
+      `badges/user_badges_search?username=${id}&order_by=${order}&normal_badges=${normal}&secret_badges=${secret}`,
+    );
 
     const { data: profile } = await api.get(
       `badges/profile_card?username=${id}`,
@@ -117,31 +126,8 @@ routes.get('/users/:id', async (request, response) => {
 
     const endIndex = Number(page) * Number(limit);
 
-    const orderBy = arrayPrincipal.sort((a: any, b: any) => {
-      if (order === 'asc') {
-        return a.count - b.count;
-      }
-
-      if (order === 'recent') {
-        const dateA = new Date(a.expires_at);
-        const dateB = new Date(b.expires_at);
-
-        return dateB.getTime() - dateA.getTime();
-      }
-
-      if (order === 'serial') {
-        return a.serial_number - b.serial_number;
-      }
-
-      return b.count - a.count;
-    });
-
     if (startIndex > arrayPrincipal.length) {
       return response.status(404).json({ message: 'Page not found' });
-    }
-
-    if (orderBy.length === 0) {
-      return response.status(404).json({ message: 'Not found' });
     }
 
     const results: any = {};
@@ -159,8 +145,6 @@ routes.get('/users/:id', async (request, response) => {
         limit: Number(limit),
       };
     }
-
-    results.results = orderBy.slice(startIndex, endIndex);
 
     results.results = arrayPrincipal.slice(startIndex, endIndex);
 
