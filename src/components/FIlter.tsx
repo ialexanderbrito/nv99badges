a370a72b08ab877c67202fde4ca95cf0ecb28542
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-25 12:06:35 -0300

    fix: remove search user

--- src/components/FIlter.tsx
+++ src/components/FIlter.tsx
@@ -1,26 +1,15 @@
-import { BiFilterAlt, BiSearch } from 'react-icons/bi';
-
-import { Pulsar } from '@uiball/loaders';
+import { BiFilterAlt } from 'react-icons/bi';
 
 import { podcastNames } from 'utils/verifyPodcast';
 
 import { useBadges } from 'contexts/Badges';
 
 export function Filter() {
-  const {
-    filter,
-    podcast,
-    setFilter,
-    setPodcast,
-    username,
-    setUsername,
-    searchUsername,
-    isLoadingPage,
-  } = useBadges();
+  const { filter, podcast, setFilter, setPodcast } = useBadges();
 
   return (
     <>
-      <div className="w-full mt-4 justify-evenly gap-2 items-center flex flex-col h-40 md:flex-row md:w-[900px] md:mt-6 md:h-20">
+      <div className="w-full mt-4 justify-start gap-2 items-center flex flex-col h-40 md:flex-row md:w-[900px] md:mt-6 md:h-20">
         <button
           className="bg-primary gap-1 text-white w-96 h-12 flex items-center justify-center rounded-md md:w-60 hover:bg-nv "
           onClick={() => {
@@ -30,37 +19,6 @@ export function Filter() {
           <BiFilterAlt />
           Filtrar por podcast
         </button>
-
-        <div className="flex items-center w-full justify-center gap-2">
-          <input
-            type="text"
-            placeholder="Pesquisar por usuário"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value.toLowerCase());
-            }}
-            onKeyPress={(e) => {
-              if (e.key === 'Enter') {
-                searchUsername();
-              }
-            }}
-            className="bg-primary text-white h-12 w-80 rounded-md px-4 outline-none focus:border border-nv sm:w-80 md:w-full"
-          />
-
-          <button
-            type="button"
-            className="bg-nv text-white w-12 h-12 flex items-center justify-center rounded-md md:w-16"
-            onClick={() => {
-              searchUsername();
-            }}
-          >
-            {isLoadingPage ? (
-              <Pulsar color="#FFF" size={24} />
-            ) : (
-              <BiSearch size={24} />
-            )}
-          </button>
-        </div>
       </div>
 
       {filter && (
