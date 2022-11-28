a550234ba2b489a51007ada8361e7fa36b26f0a9
ialexanderbrito <xanderzinho26@gmail.com>
2022-11-28 19:44:05 -0300

    feat: add card ranking and top ranking

---
+++ src/components/TopRanking.tsx
@@ -0,0 +1,59 @@
+import { avatar } from 'assets';
+import cx from 'classnames';
+import { Ranking } from 'types/BadgesProps';
+
+import { verifyVariationRanking } from 'utils/verifyPositionRanking';
+
+interface BadgeCard {
+  ranking: Ranking;
+  index: number;
+  onClick?: () => void;
+}
+
+export function TopRanking(props: BadgeCard) {
+  return (
+    <div key={props.index} className="flex flex-col">
+      <div
+        className={cx(
+          'bg-primary w-72 h-[480px] flex flex-col items-center justify-center rounded-md mb-3 overflow-hidden p-3',
+          {
+            'bg-gold': props.index === 0,
+            'bg-silver': props.index === 1,
+            'bg-bronze': props.index === 2,
+            'border border-nv': props.ranking.premium !== null,
+          },
+        )}
+      >
+        <div
+          onClick={props.onClick}
+          className="cursor-pointer flex flex-col w-full"
+        >
+          <img
+            src={props.ranking.profile_picture || avatar}
+            alt={props.ranking.username}
+            className="h-64 w-64 rounded-md bg-nv"
+            loading="lazy"
+          />
+          <div className="bg-primary flex flex-col w-full rounded-md mt-3 h-28">
+            <div className="flex w-full mt-3 items-center justify-center">
+              <p className="text-white text-xl font-bold">
+                {props.ranking.username}
+              </p>
+            </div>
+            <div className="flex w-full mt-3 ml-3">
+              <p className="text-white text-sm">
+                Emblemas resgatados: {props.ranking.qtd}
+              </p>
+            </div>
+            <div className="flex w-full mt-3 ml-3 flex-row items-center gap-2">
+              <p className="text-white text-sm">
+                Posição: {props.ranking.position}{' '}
+              </p>
+              {verifyVariationRanking(props.ranking.variation_ranking)}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
