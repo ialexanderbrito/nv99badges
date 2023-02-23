82b1fc8731351ebd3ae2d284c825e13f9f45363d
ialexanderbrito <xanderzinho26@gmail.com>
2023-02-23 14:31:27 -0300

    fix: hook favorites imports

--- src/hooks/Favorites.tsx
+++ src/hooks/Favorites.tsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from 'react';
 
-import { Badge as BadgeProps, GraphProps } from 'types/BadgesProps';
+import { Badge as BadgeProps } from 'types/BadgesProps';
 
 import { useToast } from 'contexts/Toast';
 
 export function useFavorites() {
   const { toast } = useToast();
   const [badge, setBadge] = useState<BadgeProps>();
-  const [graphResult, setGraphResult] = useState<GraphProps>();
   const [isFavorite, setIsFavorite] = useState(false);
 
   function addFavorite(badge?: BadgeProps) {
@@ -60,7 +59,5 @@ export function useFavorites() {
     isFavorite,
     badge,
     setBadge,
-    graphResult,
-    setGraphResult,
   };
 }
