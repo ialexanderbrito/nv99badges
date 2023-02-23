c0d5ffa7af6a888e7d983f87796cd4f7276a1816
ialexanderbrito <xanderzinho26@gmail.com>
2023-02-23 14:43:45 -0300

    feat: add new filter podcast

--- src/components/FIlter.tsx
+++ src/components/FIlter.tsx
@@ -1,55 +1,34 @@
-import { BiFilterAlt } from 'react-icons/bi';
-
-import { podcastNames } from 'utils/verifyPodcast';
-
 import { useBadges } from 'contexts/Badges';
 
-export function Filter() {
-  const { filter, podcast, setFilter, setPodcast } = useBadges();
+interface FilterProps {
+  podcastNames: { creator_profile_id: string; label: string }[];
+}
+
+export function Filter({ podcastNames }: FilterProps) {
+  const { podcast, setPodcast } = useBadges();
+
+  function handlePodcastChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    setPodcast(event.target.value);
+  }
 
   return (
-    <>
-      <div className="w-full mt-4 justify-start gap-2 items-center flex flex-col md:flex-row md:w-[900px] md:mt-6 md:h-20">
-        <button
-          className="bg-primary gap-1 text-white w-72 h-12 flex items-center justify-center rounded-md md:w-60 hover:bg-nv "
-          onClick={() => {
-            setFilter(!filter);
-          }}
-        >
-          <BiFilterAlt />
-          Filtrar por podcast
-        </button>
-      </div>
-
-      {filter && (
-        <div className="text-white grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5">
-          {podcastNames.map((pdc) => (
-            <label className="flex font-bold p-3 cursor-pointer text-sm">
-              <input
-                type="radio"
-                name="podcast"
-                value={podcast}
-                className="accent-nv mr-2"
-                onClick={() => {
-                  setPodcast(pdc.id);
-                }}
-              />
-              {pdc.name}
-            </label>
-          ))}
-
-          <button
-            type="button"
-            className="flex bg-primary h-10 items-center justify-center rounded-md hover:bg-nv"
-            onClick={() => {
-              setFilter(!filter);
-              window.location.reload();
-            }}
+    <div className="text-white grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 items-center mt-4">
+      <p className="font-bold underline ml-4">Filtrar por podcast:</p>
+      <select
+        className="bg-dark text-white rounded p-2 text-sm font-medium"
+        value={podcast}
+        onChange={handlePodcastChange}
+      >
+        <option value="">Todos</option>
+        {podcastNames.map((podcast) => (
+          <option
+            key={podcast.creator_profile_id}
+            value={podcast.creator_profile_id}
           >
-            Limpar filtro
-          </button>
-        </div>
-      )}
-    </>
+            {podcast.label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
