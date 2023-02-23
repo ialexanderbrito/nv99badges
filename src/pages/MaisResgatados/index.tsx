ee3ea6b499c985c2cd7d696734f3706682c6b073
ialexanderbrito <xanderzinho26@gmail.com>
2023-02-23 14:32:39 -0300

    refactor: page mais resgatados

--- src/pages/MaisResgatados/index.tsx
+++ src/pages/MaisResgatados/index.tsx
@@ -1,15 +1,14 @@
 import { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
-import { useNavigate } from 'react-router-dom';
 
 import { useQuery } from '@tanstack/react-query';
-import { Badge } from 'types/BadgesProps';
+import cx from 'classnames';
 
 import { Alert } from 'components/Alert';
-import { Button } from 'components/Button';
-import { Card } from 'components/Card';
-import { CardSkeleton } from 'components/CardSkeleton';
+import { AllBadges } from 'components/AllBadges';
 import { Filter } from 'components/FIlter';
+import { PodcastBadges } from 'components/PodcastBadges';
+import { SkeletonList } from 'components/SkeletonList';
 
 import { podcastNames } from 'utils/verifyPodcast';
 
@@ -19,133 +18,133 @@ import { useToast } from 'contexts/Toast';
 import { getBadges, getBadgesCreator } from 'services/get/badges';
 
 export function MaisResgatados() {
-  const navigate = useNavigate();
   const { toast } = useToast();
 
+  const { page, podcast, setBadges, setBadgesPodcast, setTotalBadges } =
+    useBadges();
+
   const {
-    badges,
-    badgesPodcast,
-    page,
-    podcast,
-    setBadges,
-    setBadgesPodcast,
-    setPage,
-    loadMoreBadges,
-  } = useBadges();
+    data: badgesData,
+    isLoading: isLoadingBadges,
+    isError: isErrorBadges,
+  } = useQuery(['badges', page], () => getBadges(36, page, 'desc'), {
+    onSuccess: (data) => {
+      setTotalBadges(data.data.total);
 
-  const { isLoading } = useQuery(
-    ['badges', page],
-    () => getBadges(12, page, 'desc'),
-    {
-      onSuccess: (data) => {
-        setBadges((old: Badge[]) => [...old, ...data.data.results]);
-      },
-      onError: () => {
-        toast.error('Não foram encontrados mais badges', { id: 'toast' });
-      },
-      staleTime: 100000,
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     },
-  );
+    onError: () => {
+      toast.error('Não foram encontrados mais badges', { id: 'toast' });
+    },
+    cacheTime: 1000 * 60 * 60 * 3,
+  });
 
-  useEffect(() => {
-    setBadgesPodcast([]);
-    setPage(1);
-  }, [podcast]);
-
-  useQuery(
-    ['badgesCreator', podcast, page],
-    () => getBadgesCreator(podcast, 12, page, 'desc'),
+  const {
+    data: badgesPodcastData,
+    isLoading: isLoadingPodcast,
+    isError: isErrorPodcast,
+  } = useQuery(
+    ['badgesPodcast', page],
+    async () => await getBadgesCreator(podcast, 36, page, 'desc'),
     {
       onSuccess: (data) => {
-        setBadgesPodcast((old: Badge[]) => [...old, ...data.data.results]);
+        setTotalBadges(data.data.total);
+
+        window.scrollTo({ top: 0, behavior: 'smooth' });
       },
       onError: () => {
-        toast.error('Não foram encontrados mais badges 2', { id: 'toast' });
+        toast.error(
+          `Não foram encontrados mais badges do ${
+            podcastNames.find((pod) => pod.creator_profile_id === podcast)
+              ?.label
+          }`,
+          {
+            id: 'toast',
+          },
+        );
       },
-      staleTime: 100000,
+      cacheTime: 1000 * 60 * 60 * 3,
       enabled: podcast !== '',
     },
   );
 
-  const mostraBadgesMaisResgatados = badges && podcast === '';
-  const mostraBadgesFiltrados = podcast !== '' && badgesPodcast;
+  useEffect(() => {
+    if (podcast) {
+      setBadgesPodcast(badgesPodcastData?.data.results);
+    } else {
+      setBadges(badgesData?.data.results);
+    }
+  }, [badgesData, badgesPodcastData, podcast]);
+
+  if (isErrorPodcast) {
+    return (
+      <>
+        <Alert title="Os emblemas podem demorar para carregar por conta do servidor, pois ele fica em modo hibernação para economizar recursos." />
+        <Filter podcastNames={podcastNames} />
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-white text-2xl font-bold mt-4 mb-4 text-center">
+            Não foram encontrados badges do{' '}
+            {
+              podcastNames.find((pod) => pod.creator_profile_id === podcast)
+                ?.label
+            }
+          </h1>
+        </div>
+      </>
+    );
+  }
+
+  if (isErrorBadges) {
+    return (
+      <>
+        <Alert title="Os emblemas podem demorar para carregar por conta do servidor, pois ele fica em modo hibernação para economizar recursos." />
+        <Filter podcastNames={podcastNames} />
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="text-white text-2xl font-bold mt-4 mb-4 text-center">
+            Não foram encontrados badges
+          </h1>
+        </div>
+      </>
+    );
+  }
 
   return (
     <>
       <Helmet>
-        <title>NV99 Badges | Mais Resgatados</title>
+        <title>NV99 Badges | Mais Raros</title>
       </Helmet>
+      <Alert title="Os emblemas podem demorar para carregar por conta do servidor, pois ele fica em modo hibernação para economizar recursos." />
+      <Filter podcastNames={podcastNames} />
 
-      <Alert
-        title="Os emblemas podem demorar para carregar por conta do servidor, pois
-            ele fica em modo hibernação para economizar recursos."
-      />
-      <Filter />
-
-      {mostraBadgesMaisResgatados && (
+      {isLoadingPodcast && podcast ? (
+        <SkeletonList />
+      ) : (
         <>
-          <h1 className="text-white text-2xl font-bold mt-4 mb-4">
-            Emblemas mais resgatados
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-            {badges
-              .sort((a, b) => b.count - a.count)
-              .map((badge) => (
-                <Card
-                  badge={badge}
-                  key={badge.id}
-                  onClick={() => {
-                    navigate(`/badge/${badge.code}`);
-                  }}
-                />
-              ))}
+          <div className={cx({ hidden: !podcast })}>
+            <PodcastBadges
+              title={`Emblemas mais resgatados do ${
+                podcastNames.find((pod) => pod.creator_profile_id === podcast)
+                  ?.label
+              }`}
+              badgesPodcastData={badgesPodcastData?.data.results}
+              podcast={podcast}
+            />
           </div>
         </>
       )}
 
-      {mostraBadgesFiltrados && (
+      {isLoadingBadges ? (
+        <SkeletonList />
+      ) : (
         <>
-          <h1 className="text-white text-2xl font-bold mt-4 mb-4">
-            Emblemas mais resgatados do{' '}
-            {podcastNames.find((pdc) => pdc.id === podcast)?.name}
-          </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-            {badgesPodcast
-              .sort((a, b) => b.count - a.count)
-              .map((badge) => (
-                <Card
-                  badge={badge}
-                  key={badge.id}
-                  onClick={() => {
-                    navigate(`/badge/${badge.code}`);
-                  }}
-                />
-              ))}
+          <div className={cx({ hidden: podcast })}>
+            <AllBadges
+              title="Emblemas mais resgatados"
+              badges={badgesData?.data.results}
+            />
           </div>
         </>
       )}
-
-      {isLoading && (
-        <>
-          {Array.from({ length: 2 }).map((_, index) => (
-            <>
-              <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-                <CardSkeleton key={index} />
-                <CardSkeleton key={index} />
-                <CardSkeleton key={index} />
-              </div>
-            </>
-          ))}
-        </>
-      )}
-
-      <Button
-        onClick={() => {
-          loadMoreBadges();
-        }}
-      >
-        Carregar mais
-      </Button>
     </>
   );
 }
