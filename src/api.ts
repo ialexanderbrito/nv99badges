5c85c89dcad669418cb7fdfbbf11f2b3fbaa19c4
ialexanderbrito <xanderzinho26@gmail.com>
2023-03-03 20:42:45 -0300

    feat: add cronjob 15 minutes

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import cron from 'node-cron';
 
 require('dotenv').config({
   path: '.env',
 });
+
+const TOKEN_NV = process.env.NV_KEY;
 
 export const api = axios.create({
   baseURL: 'https://stickers-nv99-2eqj3fl3la-ue.a.run.app/v2',
@@ -20,7 +23,28 @@ export const apiCreators = axios.create({
 
 export const apiNv = axios.create({
   baseURL: 'https://nv99.com.br/api',
-  headers: {
-    'x-firebase-appcheck': process.env.NV_KEY,
-  },
+});
+
+apiNv.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers['x-firebase-appcheck'] = TOKEN_NV;
+  }
+  return config;
+});
+
+cron.schedule('*/15 * * * *', () => {
+  axios
+    .get('https://nv99.com.br/api/search/content?q=Flamengo')
+    .then((response) => {
+      const headers = response.headers;
+
+      const firebaseToken = headers['x-firebase-appcheck'];
+
+      process.env.NV_KEY = firebaseToken;
+
+      console.log('Token atualizado', firebaseToken);
+    })
+    .catch(() => {
+      console.log('Erro ao atualizar token');
+    });
 });
